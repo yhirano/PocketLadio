a59aa4f02a61b49d2c7b0e5f784c9f349a8deb4a
hirano <devnull@localhost>
2006-01-10 15:18:42 +0000

    足りなかったファイルの追加

---
+++ PocketLadio/StationInterface/IChanel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PocketLadio.StationInterface
+{
+    /// <summary>
+    /// 番組インターフェース
+    /// </summary>
+    public interface IChanel
+    {
+        /// <summary>
+        /// 番組の放送URLを返す
+        /// </summary>
+        /// <returns>番組の放送URL</returns>
+        string GetPlayUrl();
+
+        /// <summary>
+        /// 番組のウェブサイトURLを返す
+        /// </summary>
+        /// <returns>番組のウェブサイトURL</returns>
+        string GetWebSiteUrl();
+
+        /// <summary>
+        /// 番組の表示方法に従って番組の情報を返す
+        /// </summary>
+        /// <returns>番組の表示方法に従った番組の情報</returns>
+        string GetChanelView();
+
+        /// <summary>
+        /// フィルタリング対象のワードを返す。
+        /// 返されたワードに従い、フィルタリングを行う。
+        /// </summary>
+        /// <returns>フィルタリング対象のワード</returns>
+        string GetFilterdWord();
+    }
+}
---
+++ PocketLadio/StationInterface/IHeadline.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PocketLadio.StationInterface
+{
+    /// <summary>
+    /// ヘッドラインインターフェース
+    /// </summary>
+    public interface IHeadline
+    {
+        /// <summary>
+        /// ヘッドラインのIDを返す
+        /// </summary>
+        /// <returns>ヘッドラインのID</returns>
+        string GetID();
+
+        /// <summary>
+        /// ヘッドラインの種類の名前を返す
+        /// </summary>
+        /// <returns>ヘッドラインの種類の名前</returns>
+        string GetKindName();
+        
+        /// <summary>
+        /// 取得している番組のリストを返す
+        /// </summary>
+        /// <returns>番組のリスト</returns>
+        IChanel[] GetChanels();
+
+        /// <summary>
+        /// ヘッドラインをネットから取得する
+        /// </summary>
+        void WebGetHeadline();
+
+        /// <summary>
+        /// ヘッドラインをネットから取得した時刻を返す
+        /// </summary>
+        /// <returns>ヘッドラインをネットから取得した時刻</returns>
+        DateTime GetLastCheckTime();
+
+        /// <summary>
+        /// ヘッドラインの設定フォームを表示する
+        /// </summary>
+        void ShowSettingForm();
+
+        /// <summary>
+        /// 番組の詳細フォームを表示する
+        /// </summary>
+        /// <param name="Chanel">番組</param>
+        void ShowPropertyForm(IChanel chanel);
+    }
+}
